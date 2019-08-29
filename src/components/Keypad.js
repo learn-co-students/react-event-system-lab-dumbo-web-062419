32ca73f9fa854d441f577891424522423978d728
Your Name <you@example.com>
2019-08-29 19:58:05 -0400

    Done.

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,21 @@
-// Code Keypad Component Here
+import React, { Component } from 'react';
+
+class Keypad extends Component {
+    
+    onKeyUp = () => {
+        console.log("Entering password...");
+        
+    }
+
+
+    render() { 
+        
+        
+        return (
+        
+            <input onKeyUp={this.onKeyUp} type="password"  />
+        )
+    }
+}
+ 
+export default Keypad;
